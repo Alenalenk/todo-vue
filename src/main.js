744fd7f794a6829c530ce7f4a92f67903f7f091b
Olena <113938017+Alenalenk@users.noreply.github.com>
2024-09-10 15:35:29 +0300

    add starting code

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import './assets/styles/index.scss';
+import './assets/styles/todo.scss';
+import './assets/styles/todoapp.scss';
+import './assets/styles/filter.scss';
+
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
